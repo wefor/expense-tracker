cb118eb177db05d0c38d1e88ecd0c5fc99df7f41
jacky <jacky.chang@cg01.net>
2025-11-24 23:06:20 +0800

    Project initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <>
+            <div className="min-h-screen flex items-start justify-start bg-gray-100">
+                <h1 className="text-4xl font-bold text-blue-600">
+                    Expense Tracker
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default App
